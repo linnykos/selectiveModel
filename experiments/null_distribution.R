64f0c46dad62e54e821c06384936ab985c1c1b8b
linnylin92 <klin1234@gmail.com>
2017-11-16 11:16:52 -0500

    Changes to null distribution simulation

--- experiments/null_distribution.R
+++ experiments/null_distribution.R
@@ -1,4 +1,5 @@
-trials <- 100
+library(selectiveModel)
+trials <- 500
 
 func <- function(i){
   write.csv(i, file = "../experiments/tmp.csv")
@@ -6,15 +7,17 @@ func <- function(i){
   y <- c(rep(0, 5), rep(5, 5)) + rnorm(10)
   fit_method <- function(y){binSegInf::binSeg_fixedSteps(y, 1)}
 
-  selected_model_inference(y, fit_method, verbose = F, cores = NA,
-                           num_samp = 500,
+  selected_model_inference(y, fit_method, verbose = T, cores = NA,
+                           num_samp = 1000,
                            param = list(burn_in = 3, seed = 1, time_limit = 600),
                            sample_method = "rejection")
 }
 
-doMC::registerDoMC(cores = 3)
+doMC::registerDoMC(cores = 14)
 res <- foreach::"%dopar%"(foreach::foreach(trial = 1:trials), func(trial))
 res <- unlist(res)
 
 plot(sort(res), seq(0, 1, length.out = length(res)), asp = T, pch = 16)
 lines(c(0,1), c(0,1), col = "red", lwd = 1)
+
+save.image("null_distribution.RData")
