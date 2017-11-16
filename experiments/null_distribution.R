a3b0e4f7c1602996aaa1f00769bfb35fb0975ac9
linnylin92 <klin1234@gmail.com>
2017-11-16 11:50:07 -0500

    Trying to test out hit-and-run

--- experiments/null_distribution.R
+++ experiments/null_distribution.R
@@ -7,10 +7,11 @@ func <- function(i){
   y <- c(rep(0, 5), rep(5, 5)) + rnorm(10)
   fit_method <- function(y){binSegInf::binSeg_fixedSteps(y, 1)}
 
-  selected_model_inference(y, fit_method, verbose = T, cores = NA,
+  res <- selected_model_inference(y, fit_method, verbose = F, cores = NA,
                            num_samp = 1000,
-                           param = list(burn_in = 3, seed = 1, time_limit = 600),
-                           sample_method = "rejection")
+                           param = list(burn_in = 3, seed = 1, 
+                           time_limit = 600))
+  res$pval
 }
 
 doMC::registerDoMC(cores = 14)
@@ -21,7 +22,7 @@ plot(sort(res), seq(0, 1, length.out = length(res)), asp = T, pch = 16)
 lines(c(0,1), c(0,1), col = "red", lwd = 1)
 
 jit <- min(diff(sort(unique(res))))
-res <- res + runif(length(res), 0, jit/10)
-ks_pval <- stats::ks.test(res, punif)
+res2 <- res + runif(length(res), 0, jit/10)
+ks_pval <- stats::ks.test(res2, punif)
 
 save.image("null_distribution.RData")
