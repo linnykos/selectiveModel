7e81518d05b603c7395be35e30ca21688f561f73
klin1234@gmail.com <klin1234@gmail.com>
2017-11-16 15:30:58 -0500

    Doing a timing test

---
+++ experiments/timing_test.R
@@ -0,0 +1,20 @@
+n_seq <- ceiling(seq(5, 100, length.out = 10)/2)*2
+time_vec <- rep(0, length(n_seq))
+
+for(n in 1:length(time_vec)){
+  cat('*')
+  set.seed(i)
+  n <- n_seq[i]
+  y <- c(rep(0, n/2), rep(5, n/2)) + rnorm(n)
+  fit_method <- function(y){binSegInf::binSeg_fixedSteps(y, 1)}
+
+  start <- proc.time()["elapsed"]
+  res <- selected_model_inference(y, fit_method, verbose = T, cores = NA,
+                                  num_samp = 100,
+                                  param = list(burn_in = 3, seed = 1,
+                                               time_limit = 600))
+  end <- proc.time()["elapsed"]
+
+  time_vec[i] <- end - start
+  save.image("timing_test.RData")
+}
