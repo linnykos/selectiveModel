37c85407c49a1b281d420a3cb1842861b8f6d774
klin1234@gmail.com <klin1234@gmail.com>
2017-11-22 02:25:13 -0500

    Fixed segments warning bug

--- experiments/uniform_2d.R
+++ experiments/uniform_2d.R
@@ -1,6 +1,7 @@
 set.seed(10)
 n <- 4
 y <- rnorm(n)
+y_org <- y
 fit <- binSegInf::binSeg_fixedSteps(y, 1)
 polyhedra <- binSegInf::polyhedra(fit)
 
@@ -32,9 +33,24 @@ for(trial in 1:(trials-1)){
   y_mat[,trial+1] <- .radians_to_data(theta, y_mat[,trial], v, w)
 }
 
-x_vec <- sin(theta_vec); y_vec <- cos(theta_vec)
-
-plot(x_vec, y_vec, asp = T)
+#x_vec <- sin(theta_vec); y_vec <- cos(theta_vec)
+#plot(x_vec, y_vec, asp = T)
 
 apply(y_mat, 2, .l2norm)
 apply(y_mat, 2, function(x){.segment_means(x, segments)})
+
+y_mat <- y_mat[,-1]
+coef_mat <- apply(y_mat, 2, function(x){
+  vec <- x - y_org
+  mat <- cbind(v, w)
+  a <- mat[c(1,3), c(1:2)]
+  b <- vec[c(1,3)]
+  coef <- solve(a, b)
+
+  #theta <- atan(b/a)
+  #if(a < 0) theta <- theta + pi
+})
+
+plot(coef_mat[1,], coef_mat[2,], asp = T)
+##WARNING: the theta's keep changing so... we need to actually compute the theta wrt the
+## original y
