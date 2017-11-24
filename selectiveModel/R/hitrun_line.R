8930fe35fc3e9e09ce762614301ea09fca195116
klin1234@gmail.com <klin1234@gmail.com>
2017-11-24 00:23:50 -0500

    Added hit run line tests

--- selectiveModel/R/hitrun_line.R
+++ selectiveModel/R/hitrun_line.R
@@ -1,5 +1,44 @@
-.sampler_hit_run_line <- function(){
+.sampler_hit_run_line <- function(gaussian, segments, polyhedra, num_samp = 100,
+                                  cores = 1, burn_in = 500, lapse = 2, verbose = F){
+  if(!is.na(cores)) {
+    doMC::registerDoMC(cores = cores)
+    num_col <- ceiling(burn_in + num_samp*lapse/cores)
+  } else {
+    num_col <- burn_in + num_samp*lapse
+  }
 
+  y <- gaussian$mean
+  n <- length(y)
+
+  func <- function(i){
+    set.seed(i)
+    mat <- matrix(0, ncol = num_col, nrow = n)
+    prev_y <- y
+
+    for(j in 1:num_col){
+      if(verbose & i == 1 & j %% floor(num_col/10) == 0) cat('*')
+      mat[,j] <- .hit_run_next_point_line(prev_y, segments, polyhedra, gaussian)
+      prev_y <- mat[,j]
+    }
+
+    mat
+  }
+
+  i <- 0 #debugging reasons
+  if(!is.na(cores)) {
+    y_mat <- do.call(cbind, foreach::"%dopar%"(foreach::foreach(i = 1:cores),
+                                               func(i)))
+  } else {
+    y_mat <- func(1)
+  }
+
+  seq_vec <- seq(burn_in, ncol(y_mat), by = lapse)
+  stopifnot(length(seq_vec) >= num_samp)
+  if(length(seq_vec) > num_samp) seq_vec <- seq_vec[1:num_samp]
+
+  y_mat <- y_mat[,seq_vec]
+  stopifnot(all(.try_polyhedra(y_mat, polyhedra)))
+  y_mat
 }
 
 #' Output the next point for hit-and-run sampler, Line
--- selectiveModel/tests/testthat/test_hitrun_line.R
+++ selectiveModel/tests/testthat/test_hitrun_line.R
@@ -53,3 +53,59 @@ test_that(".hit_run_next_point_line returns points inside the polyhedra", {
   expect_true(all(bool_vec))
 })
 
+################################
+
+## .sampler_hit_run_line is correct
+
+test_that(".sampler_hit_run_line works", {
+  set.seed(50)
+  y <- rnorm(10)
+  obj <- binSegInf::binSeg_fixedSteps(y, 2)
+  poly <- binSegInf::polyhedra(obj)
+  segments <- .segments(length(y), jump_vec = binSegInf::jumps(obj))
+  gaussian <- .gaussian(y, diag(10))
+
+  res <- .sampler_hit_run_line(gaussian, segments, poly, num_samp = 15,
+                               burn_in = 10, lapse = 2)
+
+  expect_true(is.numeric(res))
+  expect_true(is.matrix(res))
+  expect_true(all(dim(res) == c(10,15)))
+})
+
+test_that(".sampler_hit_run_line preserves mean", {
+  set.seed(10)
+  y <- rnorm(10)
+  obj <- binSegInf::binSeg_fixedSteps(y, 2)
+  poly <- binSegInf::polyhedra(obj)
+  segments <- .segments(length(y), jump_vec = binSegInf::jumps(obj))
+  gaussian <- .gaussian(y, diag(10))
+
+  res <- .sampler_hit_run_line(gaussian, segments, poly, num_samp = 15,
+                               burn_in = 10, lapse = 2)
+
+  vec <- apply(res, 2, function(x){
+    sum(abs(.segment_means(y, segments) - .segment_means(x, segments)))
+  })
+
+  expect_true(all(vec < 1e-6))
+})
+
+test_that(".sampler_hit_run_line gives samples inside the polyhedra", {
+  set.seed(10)
+  y <- rnorm(10)
+  obj <- binSegInf::binSeg_fixedSteps(y, 2)
+  poly <- binSegInf::polyhedra(obj)
+  segments <- .segments(length(y), jump_vec = binSegInf::jumps(obj))
+  gaussian <- .gaussian(y, diag(10))
+
+  res <- .sampler_hit_run_line(gaussian, segments, poly, num_samp = 15,
+                               burn_in = 10, lapse = 2)
+
+  vec <- apply(res, 2, function(x){
+    all(poly$gamma %*% x >= poly$u)
+  })
+
+  expect_true(all(vec))
+})
+
