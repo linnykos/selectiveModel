1b5a0532565dabb9f1136085234082664563b2c6
linnylin92 <klin1234@gmail.com>
2017-11-24 02:16:10 -0500

    Minor changes to null distribution

--- experiments/null_distribution.R
+++ experiments/null_distribution.R
@@ -4,13 +4,12 @@ trials <- 250
 func <- function(i){
   write.csv(i, file = "../experiments/tmp.csv")
   set.seed(i)
-  y <- c(rep(0, 2), rep(-2,3), rep(5, 5)) + rnorm(10)
+  y <- rnorm(20)
   fit_method <- function(y){binSegInf::binSeg_fixedSteps(y, 1)}
 
   res <- selected_model_inference(y, fit_method, verbose = T, cores = NA,
                            num_samp = 250,
-                           param = list(burn_in = 3, seed = 1,
-                           time_limit = 600))
+                           param = list(time_limit = 600))
   res$pval
 }
 
