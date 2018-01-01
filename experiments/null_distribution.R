e3ccd442bb1b7cbe77b29e9dc8a1c12eaced01bf
klin1234@gmail.com <klin1234@gmail.com>
2018-01-01 11:41:12 -0500

    Trying to see if the burnin and lapse make a difference

--- experiments/null_distribution.R
+++ experiments/null_distribution.R
@@ -1,9 +1,8 @@
 rm(list=ls())
-#library(selectiveModel)
+library(selectiveModel)
 trials <- 250
 
 func <- function(i){
-  write.csv(i, file = "../experiments/tmp.csv")
   print(i)
   set.seed(i)
   y <- rnorm(20)
@@ -13,18 +12,18 @@ func <- function(i){
                                   test_func = selectiveModel::segment_difference,
                                   verbose = F, cores = NA,
                                   num_samp = 250, sigma = 1, ignore_jump = 1,
-                                  param = list(time_limit = 600))
+                                  param = list(burn_in = 5000, lapse = 10, time_limit = 600))
   res$pval
 }
 
-#doMC::registerDoMC(cores = 14)
-#res <- foreach::"%dopar%"(foreach::foreach(trial = 1:trials), func(trial))
-#res <- unlist(res)
-res <- rep(NA, trials)
-for(i in 1:trials){
-  if(i %% floor(trials/10) == 0) cat('*')
-  res[i] <- func(i)
-}
+doMC::registerDoMC(cores = 14)
+res <- foreach::"%dopar%"(foreach::foreach(trial = 1:trials), func(trial))
+res <- unlist(res)
+# res <- rep(NA, trials)
+# for(i in 1:trials){
+#   if(i %% floor(trials/10) == 0) cat('*')
+#   res[i] <- func(i)
+# }
 
 plot(sort(res), seq(0, 1, length.out = length(res)), asp = T, pch = 16)
 lines(c(0,1), c(0,1), col = "red", lwd = 1)
