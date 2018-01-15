d7543a09db46c7e15e2893e3ef7a6855691681ed
klin1234@gmail.com <klin1234@gmail.com>
2018-01-15 16:52:44 -0500

    Adding some details to the plots

--- simulation/comparison_pvalue_plotter.R
+++ simulation/comparison_pvalue_plotter.R
@@ -1,21 +1,40 @@
 rm(list=ls())
-load("../simulation/comparison_pvalue.RData")
 
+#### DUMMY ####
+load("../simulation/comparison_pvalue_saturated.RData")
+zz <- res
+load("../simulation/comparison_pvalue.RData")
+for(i in 1:length(res)){
+  res[[i]][2,] <- res[[i]][1,]
+  res[[i]][1,] <- zz[[i]]
+}
+
+# load("../simulation/comparison_pvalue.RData")
+
+pdf("../figures/qq.pdf", height = 3, width = 10)
 par(mfrow = c(1, length(res)))
 for(i in 1:length(res)){
   dat <- res[[i]]
   tmp <- dat[,which(!apply(dat,2,function(x){any(is.na(x))}))]
 
-  plot(sort(tmp[1,]), seq(0, 1, length.out = length(tmp[1,])), xlim = c(0,1), ylim = c(0,1), pch = 16, asp = T, main = names(res)[i])
+  plot(sort(tmp[1,]), seq(0, 1, length.out = length(tmp[1,])), xlim = c(0,1),
+       ylim = c(0,1), pch = 16, asp = T, main = paste0("Jump size: ", paramMat[i,1]),
+       xlab = "Observed quantile", ylab = "Theoretical quantile")
   points(sort(tmp[2,]), seq(0, 1, length.out = length(tmp[2,])), pch = 16, col = "red")
+  legend("bottomright", c("Saturated", "Selected known sigma"), cex=0.8,
+         fill=c("black","red"), bty = "n")
   lines(c(0,1), c(0,1), col = "blue", lwd = 2)
 }
+graphics.off()
 
+pdf("../figures/pairwise.pdf", height = 3, width = 10)
 par(mfrow = c(1, length(res)))
 for(i in 1:length(res)){
-  plot(res[[i]][1,], res[[i]][2,], pch = 16, xlab = "Saturated", ylab = "Selected", main = names(res)[i], col = rgb(0.5, 0.5, 0.5, 0.5 ), cex = 2)
+  plot(res[[i]][1,], res[[i]][2,], pch = 16, xlab = "Saturated", ylab = "Selected",
+       main = paste0("Jump size: ", paramMat[i,1]), col = rgb(0.5, 0.5, 0.5, 0.5 ), cex = 2)
+  lines(c(0,1), c(0,1), col = "blue", lwd = 2)
 }
-
+graphics.off()
 
 par(mfrow = c(1, length(res)))
 for(i in 1:length(res)){
@@ -24,6 +43,7 @@ for(i in 1:length(res)){
 
 par(mfrow = c(1, length(res)))
 for(i in 1:length(res)){
-  plot(sort(res[[i]]), seq(0, 1, length.out = length(res[[i]])), xlim = c(0,1),
+  plot(sort(res[[i]][1,]), seq(0, 1, length.out = length(res[[i]][1,])), xlim = c(0,1),
        ylim = c(0,1), pch = 16, asp = T)
+  lines(c(0,1), c(0,1), col = "blue", lwd = 2)
 }
