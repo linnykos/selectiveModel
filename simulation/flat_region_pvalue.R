71107313d708daa530b751cb68d4fedaac702931
klin1234@gmail.com <klin1234@gmail.com>
2018-05-14 16:42:50 -0400

    Added new change to simulation. The simulation now conditions on specific y's

--- simulation/flat_region_pvalue.R
+++ simulation/flat_region_pvalue.R
@@ -8,33 +8,24 @@ rule_closure <- function(n){
     lev <- vec[1]
 
     mean_vec <- c(rep(0, floor(n/2)), rep(lev, floor(n/2)))
-    y  <- mean_vec + stats::rnorm(length(mean_vec))
+
+    while(TRUE){
+      y  <- mean_vec + stats::rnorm(length(mean_vec))
+      fit <- fit_method(dat)
+      jumps <- binSegInf::jumps(fit)
+
+      if(jumps[1] == floor(n/2) & jumps[2] > jumps[1]) break()
+    }
+
     y
   }
 }
-
-find_jump <- function(y, fit){
-  n <- length(y)
-  jumps <- sort(binSegInf::jumps(fit))
-  jumps_all <- c(0,jumps,n)
-  true_split <- floor(n/2)
-
-  idx_before <- jumps_all[jumps_all <= true_split]
-  idx_after <- jumps_all[jumps_all > true_split]
-
-  stopifnot(length(idx_before) >= 3 | length(idx_after) >= 3)
-
-  if(length(idx_before) > length(idx_after)) val <- idx_before[2] else val <- idx_after[2]
-
-  which(jumps == val)
-}
-
 
 criterion_closure <- function(fit_method,
                               test_func = selectiveModel::segment_difference, num_samp = 2000, cores = NA){
   function(dat, vec, y, ...){
     fit <- fit_method(dat)
-    ignore_jump <- find_jump(dat, fit)
+    ignore_jump <- 2
 
     known_sigma <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
                                             num_samp = num_samp, ignore_jump = ignore_jump, cores = cores, verbose = F, sigma = 1, param = list(burn_in = 2000, lapse = 1))
@@ -52,7 +43,7 @@ criterion_closure <- function(fit_method,
 n <- 20
 trials <- 500
 paramMat <- as.matrix(1)
-fit_method <- function(x){binSegInf::binSeg_fixedSteps(x, numSteps = 3)}
+fit_method <- function(x){binSegInf::binSeg_fixedSteps(x, numSteps = 2)}
 
 rule <- rule_closure(n)
 criterion <- criterion_closure(fit_method)
