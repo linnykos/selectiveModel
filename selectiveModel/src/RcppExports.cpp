ac5bd3360a2580e6ffb46339a3bf66b4d5f1e74a
klin1234@gmail.com <klin1234@gmail.com>
2018-06-05 16:44:25 -0400

    Migrated the files into the correct package
    The Rcpp::Rcpp.package.skeleton function does not work if the package already
      exists...

--- selectiveModel/src/RcppExports.cpp
+++ selectiveModel/src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // rcpp_hello_world
 List rcpp_hello_world();
-RcppExport SEXP _selectiveModel_rcpp_hello_world() {
+RcppExport SEXP _anRpackage_rcpp_hello_world() {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -17,11 +17,11 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_selectiveModel_rcpp_hello_world", (DL_FUNC) &_selectiveModel_rcpp_hello_world, 0},
+    {"_anRpackage_rcpp_hello_world", (DL_FUNC) &_anRpackage_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_selectiveModel(DllInfo *dll) {
+RcppExport void R_init_anRpackage(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
