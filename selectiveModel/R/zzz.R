03b0ded5eebff2accb72948750a4f5f2a931ae50
klin1234@gmail.com <klin1234@gmail.com>
2018-08-18 15:22:59 -0400

    Still trying to fix the warning for Rcpp_Plane...

--- selectiveModel/R/zzz.R
+++ selectiveModel/R/zzz.R
@@ -1,1 +1,15 @@
 Rcpp::loadModule("module", TRUE)
+
+#' Some unneeded documentation
+#'
+#' @docType methods
+#' @name Rcpp_Plane
+#' @rdname Rcpp_Plane
+#' @export
+NULL
+
+#' Some unneeded documentation
+#'
+#' @name Plane
+NULL
+
