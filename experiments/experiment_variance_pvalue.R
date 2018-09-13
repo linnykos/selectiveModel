595bdee70479a1613c2610f2c6d9e6d1e9daa4f2
klin1234@gmail.com <klin1234@gmail.com>
2018-09-13 10:18:22 -0400

    Updated experiment

--- experiments/experiment_variance_pvalue.R
+++ experiments/experiment_variance_pvalue.R
@@ -11,46 +11,51 @@ test_func <- selectiveModel::segment_difference
 num_samp <- 2000
 cores <- NA
 
-# trials <- 100
-# # run selective model test for known sigma, testing the first jump (aka: jump with the smallest index)
-# res_vec_known_0 <- rep(NA, trials)
-# for(x in 1:trials){
-#   print(x)
-#   set.seed(x*10)
-#   res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
-#                            num_samp = num_samp, ignore_jump = 1, sigma = 1,
-#                            cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
-#   res_vec_known_0[x] <- res$pval
-# }
-# save.image("experiment_variance_pvalue.RData")
-#
-# res_vec_unknown_0 <- rep(NA, trials)
-# for(x in 1:trials){
-#   print(x)
-#   set.seed(x*10)
-#   res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
-#                                   num_samp = num_samp, ignore_jump = 1,
-#                                   cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
-#   res_vec_unknown_0[x] <- res$pval
-# }
-# save.image("experiment_variance_pvalue.RData")
+trials <- 100
+# run selective model test for known sigma, testing the first jump (aka: jump with the smallest index)
+res_vec_known_0 <- rep(NA, trials)
+for(x in 1:trials){
+  print(x)
+  set.seed(x*10)
+  res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
+                           num_samp = num_samp, ignore_jump = 1, sigma = 1,
+                           cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
+  res_vec_known_0[x] <- res$pval
+}
+save.image("experiment_variance_pvalue.RData")
+
+res_vec_unknown_0 <- rep(NA, trials)
+for(x in 1:trials){
+  print(x)
+  set.seed(x*10)
+  res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
+                                  num_samp = num_samp, ignore_jump = 1,
+                                  cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
+  res_vec_unknown_0[x] <- res$pval
+}
+save.image("experiment_variance_pvalue.RData")
 
 
 #######################
 
 set.seed(10)
 n <- 200
-k <- 1
-lev <- 0
-dat <- rnorm(n, c(rep(0, n/5), rep(lev, n/5), rep(0, n/5), rep(-lev, n/5), rep(0, n/5)))
+k <- 4
+lev <- 1
 fit_method <- function(x){binseginf::bsfs(x, numSteps = k)}
+while(TRUE){
+  dat <- rnorm(n, c(rep(0, n/5), rep(lev, n/5), rep(0, n/5), rep(-lev, n/5), rep(0, n/5)))
+  res <- fit_method(dat)
+  idx <- sort(binseginf::jumps(res))
+  if(abs(idx[1] - n/5) <= 2) break()
+}
 test_func <- selectiveModel::segment_difference
 num_samp <- 2000
 cores <- NA
 
 trials <- 100
 # run selective model test for known sigma, testing the first jump (aka: jump with the smallest index)
-res_vec_known_4 <- rep(NA, trials)
+res_vec_known_signal <- rep(NA, trials)
 for(x in 1:trials){
   print(x)
   set.seed(x*10)
@@ -59,9 +64,9 @@ for(x in 1:trials){
                                   cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
   res_vec_known_4[x] <- res$pval
 }
-save.image("experiment_variance_pvalue2.RData")
+save.image("experiment_variance_pvalue.RData")
 
-res_vec_unknown_4 <- rep(NA, trials)
+res_vec_unknown_signal <- rep(NA, trials)
 for(x in 1:trials){
   print(x)
   set.seed(x*10)
@@ -71,4 +76,4 @@ for(x in 1:trials){
   res_vec_unknown_4[x] <- res$pval
 }
 
-save.image("experiment_variance_pvalue2.RData")
+save.image("experiment_variance_pvalue.RData")
