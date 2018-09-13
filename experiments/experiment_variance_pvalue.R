298a338930b1f824d2d6df1b17fd93324fa44e31
klin1234@gmail.com <klin1234@gmail.com>
2018-09-13 07:11:00 -0400

    Changes to pvalue experiment (bug fix)

--- experiments/experiment_variance_pvalue.R
+++ experiments/experiment_variance_pvalue.R
@@ -11,39 +11,39 @@ test_func <- selectiveModel::segment_difference
 num_samp <- 2000
 cores <- NA
 
-trials <- 100
-# run selective model test for known sigma, testing the first jump (aka: jump with the smallest index)
-res_vec_known_0 <- rep(NA, trials)
-for(x in 1:trials){
-  print(x)
-  set.seed(x*10)
-  res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
-                           num_samp = num_samp, ignore_jump = 1, sigma = 1,
-                           cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
-  res_vec_known_0[x] <- res$pval
-}
-save.image("experiment_variance_pvalue.RData")
-
-res_vec_unknown_0 <- rep(NA, trials)
-for(x in 1:trials){
-  print(x)
-  set.seed(x*10)
-  res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
-                                  num_samp = num_samp, ignore_jump = 1,
-                                  cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
-  res_vec_unknown_0[x] <- res$pval
-}
-save.image("experiment_variance_pvalue.RData")
+# trials <- 100
+# # run selective model test for known sigma, testing the first jump (aka: jump with the smallest index)
+# res_vec_known_0 <- rep(NA, trials)
+# for(x in 1:trials){
+#   print(x)
+#   set.seed(x*10)
+#   res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
+#                            num_samp = num_samp, ignore_jump = 1, sigma = 1,
+#                            cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
+#   res_vec_known_0[x] <- res$pval
+# }
+# save.image("experiment_variance_pvalue.RData")
+#
+# res_vec_unknown_0 <- rep(NA, trials)
+# for(x in 1:trials){
+#   print(x)
+#   set.seed(x*10)
+#   res <- selected_model_inference(dat, fit_method = fit_method, test_func = test_func,
+#                                   num_samp = num_samp, ignore_jump = 1,
+#                                   cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
+#   res_vec_unknown_0[x] <- res$pval
+# }
+# save.image("experiment_variance_pvalue.RData")
 
 
 #######################
 
 set.seed(10)
 n <- 200
-k <- 4
+k <- 1
 lev <- 0
 dat <- rnorm(n, c(rep(0, n/5), rep(lev, n/5), rep(0, n/5), rep(-lev, n/5), rep(0, n/5)))
-fit_method <- function(x){binseginf::fl(x, numSteps = k)}
+fit_method <- function(x){binseginf::bsfs(x, numSteps = k)}
 test_func <- selectiveModel::segment_difference
 num_samp <- 2000
 cores <- NA
@@ -59,7 +59,7 @@ for(x in 1:trials){
                                   cores = cores, verbose = F, param = list(burn_in = 2000, lapse = 1))
   res_vec_known_4[x] <- res$pval
 }
-save.image("experiment_variance_pvalue.RData")
+save.image("experiment_variance_pvalue2.RData")
 
 res_vec_unknown_4 <- rep(NA, trials)
 for(x in 1:trials){
@@ -71,4 +71,4 @@ for(x in 1:trials){
   res_vec_unknown_4[x] <- res$pval
 }
 
-save.image("experiment_variance_pvalue.RData")
+save.image("experiment_variance_pvalue2.RData")
