0b538e9164da084230d5b2c664af0ce685b65965
klin1234@gmail.com <klin1234@gmail.com>
2018-09-17 00:06:15 -0400

    Wrote the main powercuve experiment

--- experiments/experiment_jump_counter_analysis.R
+++ experiments/experiment_jump_counter_analysis.R
@@ -6,8 +6,33 @@ bs_count <- sapply(bs_res, function(x){
   x <- apply(x, 2, sort, decreasing = F)
   sum(sapply(1:2, function(i){length(which(abs(x[i,] - true_jumps[i]) <= 2))}))
 })
+bs_count
 
 fl_count <- sapply(fl_res, function(x){
   x <- apply(x, 2, sort, decreasing = F)
   sum(sapply(1:2, function(i){length(which(abs(x[i,] - true_jumps[i]) <= 2))}))
 })
+fl_count
+
+##############
+
+bs_trials <- sapply(bs_res, function(x){
+  x <- apply(x, 2, sort, decreasing = F)
+  seq_vec <- c(1:120)*100
+  count <- sapply(seq_vec, function(k){
+    sum(sapply(1:2, function(i){length(which(abs(x[i,1:k] - true_jumps[i]) <= 2))}))
+  })
+  seq_vec[which.min(abs(count - 2000))]
+})
+bs_trials # 24000, 6600, 2200, 1300, 1100
+
+fl_trials <- sapply(fl_res, function(x){
+  x <- apply(x, 2, sort, decreasing = F)
+  seq_vec <- c(1:120)*100
+  count <- sapply(seq_vec, function(k){
+    sum(sapply(1:2, function(i){length(which(abs(x[i,1:k] - true_jumps[i]) <= 2))}))
+  })
+  seq_vec[which.min(abs(count - 2000))]
+})
+fl_trials # 13000, 6100, 2900, 1800, 1300
+
---
+++ experiments/main_powercurve.R
@@ -0,0 +1,72 @@
+rm(list=ls())
+library(simulation)
+library(binseginf)
+library(selectiveModel)
+
+paramMat <- cbind(c(.25,.5,1,2,4), 200)
+colnames(paramMat) <- c("SnR", "n")
+
+paramMat_bs <- cbind(paramMat, c(24000, 6600, 2200, 1300, 1100))
+colnames(paramMat_bs)[3] <- "trials"
+paramMat_fl <- cbind(paramMat, c(13000, 6100, 2900, 1800, 1300))
+colnames(paramMat_fl)[3] <- "trials"
+
+middle_mutation <- function(lev, n){
+  mn <- rep(0,n)
+  mn[seq(from=n/2+1, to=n/2+round(.2*n))] <- lev
+  mn
+}
+true_jumps <- c(100, 140)
+test_func <- selectiveModel::segment_difference
+num_samp <- 4000
+burn_in <- 4000
+
+rule <- function(vec){
+  middle_mutation(lev = vec["SnR"], n = vec["n"]) + stats::rnorm(vec["n"])
+}
+
+criterion_closure <- function(fit_method){
+  function(dat, vec, y){
+    fit <- fit_method(dat)
+    jump_vec <- sort(binseginf::jumps(fit))
+
+    res <- c(jump_vec, rep(NA, 2))
+    names(res) <- c("Jump 1", "Jump 2", "Pvalue 1", "Pvalue 2")
+
+    for(i in 1:2){
+      if(abs(jump_vec[i] - true_jumps[i]) <= 2){
+        set.seed(10*y)
+        tmp <- selected_model_inference(dat, fit_method = fit_method,
+                                        test_func = test_func, num_samp = num_samp,
+                                        ignore_jump = i, sigma = 1, cores = cores,
+                                        verbose = F, param = list(burn_in = burn_in,
+                                                                  lapse = 1))
+        res[i+2] <- tmp$pval
+      }
+    }
+
+    res
+  }
+}
+
+fit_method_bs <- function(x){binseginf::bsfs(x, numSteps = 2)}
+fit_method_fl <- function(x){binseginf::fLasso_fixedSteps(x, numSteps = 2)}
+
+criterion_bs <- criterion_closure(fit_method_bs)
+criterion_fl <- criterion_closure(fit_method_fl)
+
+###########################
+
+bs_res <- simulation::simulation_generator(rule = rule, criterion = criterion_bs,
+                                           paramMat = paramMat_bs, trials = paramMat_bs[,"trials"],
+                                           cores = 15, as_list = F,
+                                           filepath = "experiment_jump_counter_tmp.RData",
+                                           verbose = T)
+save.image("experiment_jump_counter.RData")
+
+fl_res <- simulation::simulation_generator(rule = rule, criterion = criterion_fl,
+                                           paramMat = paramMat_fl, trials = paramMat_fl[,"trials"],
+                                           cores = 15, as_list = F,
+                                           filepath = "experiment_jump_counter_tmp.RData",
+                                           verbose = T)
+save.image("experiment_jump_counter.RData")
