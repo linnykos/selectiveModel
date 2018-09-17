77ee24fa2d67810a8d82a38f9e759500ceb6bd4c
klin1234@gmail.com <klin1234@gmail.com>
2018-09-16 22:45:25 -0400

    New test to compute the number of trials needed

---
+++ experiments/experiment_jump_counter.R
@@ -0,0 +1,41 @@
+rm(list=ls())
+library(simulation)
+library(binseginf)
+
+paramMat <- cbind(c(.25,.5,1,2,4), 200)
+colnames(paramMat) <- c("SnR", "n")
+middle_mutation <- function(lev, n){
+  mn <- rep(0,n)
+  mn[seq(from=n/2+1, to=n/2+round(.2*n))] <- lev
+  mn
+}
+
+rule <- function(vec){
+  middle_mutation(lev = vec["SnR"], n = vec["n"]) + stats::rnorm(vec["n"])
+}
+
+criterion_closure <- function(method){
+  function(dat, vec, y){
+    fit <- method(dat)
+    binseginf::jumps(fit)
+  }
+}
+
+criterion_bs <- criterion_closure(function(x){binseginf::bsfs(x, 2)})
+criterion_fl <- criterion_closure(function(x){binseginf::fLasso_fixedSteps(x, 2)})
+
+########################
+
+bs_res <- simulation::simulation_generator(rule = rule, criterion = criterion_bs,
+                                           paramMat = paramMat, trials = 9000,
+                                           cores = 15, as_list = F,
+                                           filepath = "experiment_jump_counter_tmp.RData",
+                                           verbose = T)
+save.image("experiment_jump_counter.RData")
+
+fl_res <- simulation::simulation_generator(rule = rule, criterion = criterion_fl,
+                                           paramMat = paramMat, trials = 9000,
+                                           cores = 15, as_list = T,
+                                           filepath = "experiment_jump_counter_tmp.RData",
+                                           verbose = T)
+save.image("experiment_jump_counter.RData")
