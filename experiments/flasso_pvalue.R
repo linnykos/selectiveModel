e4795e1330faf32006900abf0ad6085a1aa58b11
klin1234@gmail.com <klin1234@gmail.com>
2018-09-25 23:30:20 -0400

    Edits to flasso. The contrast function is wrong

--- experiments/flasso_pvalue.R
+++ experiments/flasso_pvalue.R
@@ -9,6 +9,17 @@ middle_mutation <- function(lev, n){
 }
 true_jumps <- c(100, 140)
 
+.form_contrast <- function(jump_vec, n, location){
+  jump_vec <- sort(unique(c(0, jump_vec, n)))
+  contrast_vec <- rep(0, n)
+
+  contrast_vec[(jump_vec[location]+1):jump_vec[location+1]] <- -1/(jump_vec[location+1] - (jump_vec[location]+1) + 1)
+  contrast_vec[(jump_vec[location+1]+1):jump_vec[location+2]] <- 1/(jump_vec[location+2] - (jump_vec[location+1]+1) + 1)
+
+  contrast_vec
+}
+
+n <- 200
 numSteps <- 2
 num_samp <- 4000
 burn_in <- 1000
@@ -17,18 +28,18 @@ fit_method <- function(x){binseginf::fLasso_fixedSteps(x, numSteps = numSteps)}
 
 
 set.seed(10)
-counter <- 103
+counter <- 289
 while(TRUE){
   print(counter)
   set.seed(10 * counter)
-  y <- middle_mutation(lev = 4, n = 200) + rnorm(200)
+  y <- middle_mutation(lev = 4, n = n) + rnorm(n)
   fit <- fit_method(y)
   poly <- binseginf::polyhedra(fit)
   jump_vec <- sort(binseginf::jumps(fit))
 
   for(i in 1:numSteps){
     if(any(abs(jump_vec[i] - true_jumps) <= 2)){
-      contrast <- as.matrix(binseginf::contrast_vector(fit, i))
+      contrast <- .form_contrast(jump_vec, n, i)
       sat_pval <- binseginf::pvalue(y, poly, contrast, alternative = "one.sided")
 
       if(!is.nan(sat_pval) && sat_pval >= 0.1) {
@@ -43,6 +54,8 @@ while(TRUE){
                                                                                   lapse = 1),
                                                         return_samples = T)
         sel_pval <- res$pval
+
+        stopifnot(abs(contrast - res$test_stat) <= 1e-6)
 
         if(sel_pval <= 0.05/2) {
           save.image("flasso_pvalue.RData")
---
+++ experiments/flasso_pvalue_analysis.R
@@ -0,0 +1,2 @@
+rm(list=ls())
+load("../experiments/flasso_pvalue.RData")
