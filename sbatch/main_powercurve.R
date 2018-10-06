d3d16490dbdf83362316fcac462d15744a6a4677
klin1234@gmail.com <klin1234@gmail.com>
2018-10-06 17:52:26 -0400

    Pushing the first script for sbatch

--- sbatch/main_powercurve.R
+++ sbatch/main_powercurve.R
@@ -6,21 +6,23 @@ library(selectiveModel, lib.loc = "/home/kevinl1/Rpackages/3.5")
 args <- commandArgs(trailingOnly=TRUE)
 #arguments:
 ## - 1st: type of signal (1 = 1-jump, 2 = 2-jump)
-## - 2nd: signal to noise ratio (0, 0.25, 0.5, 1, 2, 4)
+## - 2nd: signal to noise ratio, integers from 1 to 6 for (0, 0.25, 0.5, 1, 2, 4)
 ## - 3th: method (1 = bs, 2 = fl)
-## - 4th: sigma (either 1 or NA)
+## - 4th: sigma (either 1 or 2 or 1 or NA)
 ## - 5th: ksteps (2 to 4)
 ## - 6th: decluttered (0 = no, 1 = yes)
 ## - 7th: trials
 
 paramMat <- matrix(args, ncols = length(args))
 colnames(paramMat) <- c("Type", "SnR", "method", "sigma", "ksteps", "decluttered", "trials")
+paramMat[,"SnR"] <- c(0, 0.25, 0.5, 1, 2, 4)[paramMat[,"SnR"]]
+paramMat[,"sigma"] <- c(1,NA)[paramMat[,"sigma"]]
 args <- paramMat[1,]
+
 
 ######
 num_samp <- 4000
 burn_in <- 4000
-numSteps <- 4
 n <- 200
 
 edge_mutation <- function(lev, n=200){
@@ -75,11 +77,11 @@ criterion_closure <- function(fit_method){
                                                 desired_jumps = true_jumps)
     }
 
-    res <- rep(NA, 3*numSteps)
+    res <- rep(NA, 3*vec["ksteps"])
     len <- length(cluster_list$jump_vec)
     res[1:len] <- cluster_list$jump_vec
-    names(res) <- c(paste0("Jump ", 1:numSteps), paste0("Direction ", 1:numSteps),
-                    paste0("Pvalue ", 1:numSteps))
+    names(res) <- c(paste0("Jump ", 1:vec["ksteps"]), paste0("Direction ", 1:vec["ksteps"]),
+                    paste0("Pvalue ", 1:vec["ksteps"]))
 
     for(i in 1:len){
       if(cluster_list$target_bool[i]){
@@ -103,8 +105,8 @@ criterion_closure <- function(fit_method){
                                         sigma = vec["sigma"],
                                         verbose = F, param = list(burn_in = burn_in,
                                                                   lapse = 1))
-        res[i+numSteps] <- direction
-        res[i+2*numSteps] <- tmp$pval
+        res[i+vec["ksteps"]] <- direction
+        res[i+2*vec["ksteps"]] <- tmp$pval
       }
     }
     res
@@ -124,4 +126,4 @@ criterion <- criterion_closure(fit_method)
 res <- simulation::simulation_generator(rule = rule, criterion = criterion,
                                            paramMat = paramMat, trials = paramMat[,"trials"],
                                            cores = 28, as_list = F, verbose = T)
-save.image(paste0("main_powercurve", paste0(args, collapse = "-"), ".RData"))
+save.image(paste0("/home/kevinl1/selectivemodel/sbatch/results/main_powercurve", paste0(args, collapse = "-"), ".RData"))
