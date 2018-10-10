c2754350fa458364ffc1746617d6a114e714f488
klin1234@gmail.com <klin1234@gmail.com>
2018-10-10 14:45:26 -0400

    Fixed another typo

--- sbatch/demo_experiments/Rarray.sh
+++ sbatch/demo_experiments/Rarray.sh
@@ -2,4 +2,4 @@
 
 module load R/3.5.1-mkl
 
-sbatch --export arg1=2 --array=0-10%3 /home/kevinl1/selectivemodel/sbatch/demo_experiments/nodename.R
+sbatch --export arg1=2 --array=0-10%3 /home/kevinl1/selectivemodel/sbatch/demo_experiments/Rarray.sbatch
